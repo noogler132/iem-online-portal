19ab6c38edeb983a952a649dc33cdd842ef900e2
Sachin Agarwal <sachinagarwal132@gmail.com>
2018-03-30 20:27:07 +0000

    app.js edited online with Bitbucket

--- iem-nodejs/app.js
+++ iem-nodejs/app.js
@@ -1,15 +1,16 @@
+/* REQUIRED MODULES */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
+// var mysql = require('mysql');
 var passport = require('passport');
 var session = require('express-session');
 var flash    = require('connect-flash');
 
-
+/* APP ROUTES */
 var index = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
@@ -19,9 +20,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// require('./config/passport')(passport); // pass passport for configuration
+/* PASSPORT CONFIG */
+require('./config/passport')(passport); // pass passport for configuration
 
-// uncomment after placing your favicon in /public
+/* DATABASE CONFIG */
+db = require('./config/dbconfig');
+
+
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -29,9 +34,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* APP ROUTES URLS */
 app.use('/', index);
 app.use('/users', users);
 app.use('/login', login);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,6 +47,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -50,21 +59,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-// db = mysql.createConnection({
-//     host: 'johnny.heliohost.org',
-//     user: 'iemweb_141132',
-//     password: 'password14131',
-//     database: 'iemweb_database'
-// });
-//
-// db.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-// });
-
-//console.log(con);
-//app.con = con;
 
 // required for passport
 app.use(session({ secret: 'iemwebportal', resave: true, saveUninitialized: true })); // session secret
