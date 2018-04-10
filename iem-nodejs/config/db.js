04ed93d805e1d2980883c156f525b0fcf38690c5
SumanKanrar-IEM <sumankanrar420@gmail.com>
2018-04-10 21:04:30 +0530

    changes made by sachin

--- iem-nodejs/config/db.js
+++ iem-nodejs/config/db.js
@@ -1,27 +1,26 @@
 var db = require('./dbconfig');
-var bcrypt = require('bcrypt');
 
 var u = '10401215076';
-// db.query("SELECT * FROM student_auth WHERE u_roll = ?", u, function (err, result) {
-//     if (err) throw err;
-//     var h = bcrypt.hashSync(result.password, bcrypt.genSaltSync(8), null);
-//     console.log(h);
-//     db.query("UPDATE student_auth SET password = ? WHERE u_roll = ?", h , u, function (err, r) {
-//         if (err) throw err;
-//         console.log(r);
-//     } )
-// });
+db.query("SELECT * FROM auth WHERE u_id = ?", u, function (err, result, fields) {
+    if (err) throw err;
+    // var h = bcrypt.hashSync(result.password, bcrypt.genSaltSync(8), null);
+    // console.log(h);
+    // db.query("UPDATE student_auth SET password = ? WHERE u_roll = ?", h , u, function (err, r) {
+    //     if (err) throw err;
+    //     console.log(r);
+    console.log(fields);
+} );
 
 
 
-    bcrypt.hash('00000000', 8, function(err, hash) {
-        // Store hash in your password DB.
-        db.query("UPDATE student_auth SET password = ? WHERE u_roll = 10401215076", hash, function (err, r) {
-            if (err) throw err;
-            console.log(r);
-        })
-
-    });
+    // bcrypt.hash('00000000', 8, function(err, hash) {
+    //     // Store hash in your password DB.
+    //     db.query("UPDATE student_auth SET password = ? WHERE u_roll = 10401215076", hash, function (err, r) {
+    //         if (err) throw err;
+    //         console.log(r);
+    //     })
+    //
+    // });
 
 
 
