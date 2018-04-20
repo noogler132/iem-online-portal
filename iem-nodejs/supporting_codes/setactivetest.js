1582bd8600877d3352daaf80311106427234f6eb
Sachin Agarwal <sachinagarwal132@gmail.com>
2018-04-21 02:09:30 +0530

    test paper upload done

--- iem-nodejs/supporting_codes/setactivetest.js
+++ iem-nodejs/supporting_codes/setactivetest.js
@@ -1,8 +1,13 @@
 module.exports = function (subcode, testnumber) {
-    db.query("SELECT * FROM active_tests WHERE sub_code = ? AND test_no = ?", subcode, testnumber, function (err, result, fields) {
+// var db = require('../config/dbconfig');
+// var subcode = 'BCA101';
+// var testnumber = '1';
+    db.query('SELECT * FROM active_tests WHERE sub_code = ? AND test_no = ? ', [subcode, testnumber] , function (err, result, fields) {
+        console.log('not even inside');
         if(result.length === 0){
-            db.query(" INSERT INTO `active_tests`(`sub_code`, `test_no`, `is_active`, `test_key`) VALUES (?,?,?,?)", subcode,
-                testnumber, 1, subcode+'_'+testnumber, function (err, result, fields) {
+            console.log('inside if');
+            db.query(" INSERT INTO `active_tests`(`sub_code`, `test_no`, `is_active`, `test_key`) VALUES (?,?,?,?)", [subcode,
+                testnumber, 1, subcode+'_'+testnumber], function (err, result, fields) {
                 if(err) throw err;
                 });
         }
