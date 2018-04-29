ee34046fa3712a3aa7868d2db96326ee3c0569be
Sachin Agarwal <sachinagarwal132@gmail.com>
2018-04-29 17:33:55 +0530

    working on superAdmin

--- iem-nodejs/routes/superAdmin.js
+++ iem-nodejs/routes/superAdmin.js
@@ -11,8 +11,25 @@ router.get('/', function(req, res, next) {
     res.render('superAdmin/sections', {});
 });
 
+/* GET ADD USER */
 router.get('/add-user', function(req, res, next) {
     res.render('superAdmin/add-user', {});
 });
+
+/* GET STUDENT DETAILS */
+router.get('/students', function(req, res, next) {
+    res.render('superAdmin/student-table', {});
+});
+
+/* GET TEACHERS DETAILS */
+router.get('/teachers', function(req, res, next) {
+    res.render('superAdmin/teacher-table', {});
+});
+
+
+router.get('/subjects', function(req, res, next) {
+    res.render('superAdmin/subject-table', {});
+});
+
 
 module.exports=router;
