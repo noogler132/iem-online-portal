71e42ea7611e69e5a55fe9831ef948b09d1be040
Sachin Agarwal <sachinagarwal132@gmail.com>
2018-05-01 12:46:16 +0530

    completed student registration

--- iem-nodejs/routes/register.js
+++ iem-nodejs/routes/register.js
@@ -13,18 +13,35 @@ router.get('/', function(req, res, next) {
         res.redirect('/login');
     }
     else {
-        db.query("SELECT * FROM auth WHERE u_id = ?", req.session.uid, function (err, result, fields) {
-            if (err) throw err;
-            res.render('login/register', {title: 'IEM', user: user, email: result[0].email, u_id: result[0].u_id});
-        });
+            res.render('login/register', {err: '', title: 'IEM', user: user, email: req.session.email, u_id: req.session.uid});
     }
 });
 
 /* POST registration page. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
     var user = checkSession(req);
+    var u_id = req.session.uid;
+    var email = req.session.email;
+    var u_reg = req.body.u_reg;
+    var f_name = req.body.f_name;
+    var l_name = req.body.l_name;
+    var dob = req.body.dob;
+    var dept = req.body.dept;
+    var add_year = req.body.add_year;
+    var contact = req.body.contact;
 
+    if( u_id === '' || email === '' || u_reg === '' || f_name === '' || l_name === '' || dob === '' || dept === '' || add_year === '' || contact === ''){
+        res.render('login/register', {err: 'All fields are mandatory', title: 'IEM', user: user, email: req.session.email, u_id: req.session.uid});
+        return;
+    }
+    var arr =[u_id, email, u_reg, f_name, l_name, dob, dept, add_year, contact];
+    db.query("Insert into student_details(u_roll, email, u_reg, f_name, l_name, dob, dept, add_year, contact) values (?,?,?,?,?,?,?,?,?)",
+        arr, function (err, result, fields) {
+        if (err) throw err;
+        res.redirect('/');
     });
+
+});
 
 
 module.exports = router;
